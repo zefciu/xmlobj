d0805ea790eeb2dbac8e22b6d16bd29ce3b15bbb
Szymon Pyżalski <szymon@pythonista.net>
2013-10-13 10:25:55 +0200

    One test for phase 4 written

---
+++ xmlobj/test_attribute.py
@@ -0,0 +1,32 @@
+"""Tests for attribute access."""
+
+import unittest
+from lxml import etree
+
+from xmlobj import XmlObj
+
+INITIAL_DATA = """<person type="user">
+    <name>Galahad</name>
+    <nickname>The Pure</nickname>
+    <age>31</age>
+</person>"""
+
+class TestAttribute(unittest.TestCase):
+    """Test the attribute access of xmlobj."""
+
+    def setUp(self):
+        """Create an object with test data."""
+
+        class Person(XmlObj):
+            """A person."""
+            type = Attribute()
+            source = Attribute()
+
+        self.obj = User()
+        self.obj._element = etree.fromstring(INITIAL_DATA)
+
+    def test_getattr(self):
+        """Test the getattribute behaviour."""
+        self.assertEqual(self.obj.type, 'user')
+
+
